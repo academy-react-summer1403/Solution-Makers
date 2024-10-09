71fdda1ffa66301199e385223efc8eacd2042877
mhamddyn07 <139611782+mhamddyn07@users.noreply.github.com>
2024-10-09 13:46:09 +0330

    Update index.jsx

--- src/components/common/Navbar/index.jsx
+++ src/components/common/Navbar/index.jsx
@@ -7,15 +7,23 @@ import {
   Badge,
 } from "@nextui-org/react";
 
-import { useContext, useState } from "react";
+import {
+  useContext,
+  useState,
+} from "react";
 import { AppContext } from "../../../context/Provider";
-import { NavLink, Link } from "react-router-dom";
+import {
+  NavLink,
+  Link,
+} from "react-router-dom";
 import NavMenuLink from "./NavMenuLink";
 import "./index.css";
 
 function MyNavbar() {
-  const [isMenuOpen, setIsMenuOpen] = useState(false);
-  const { bagIconNum } = useContext(AppContext);
+  const [isMenuOpen, setIsMenuOpen] =
+    useState(false);
+  const { bagIconNum } =
+    useContext(AppContext);
 
   return (
     <Navbar
@@ -27,7 +35,11 @@ function MyNavbar() {
     >
       <div className="w-full h-[50px] flex justify-start sm:justify-between sm:px-4 items-center gap-2">
         <NavbarMenuToggle
-          aria-label={isMenuOpen ? "Close menu" : "Open menu"}
+          aria-label={
+            isMenuOpen
+              ? "Close menu"
+              : "Open menu"
+          }
           className="md:hidden"
         />
         <Link to="/">
@@ -38,22 +50,34 @@ function MyNavbar() {
         </Link>
         <ul className="hidden md:flex gap-12">
           <li>
-            <NavLink to="/courses">دوره‌ها</NavLink>
+            <NavLink to="/courses">
+              دوره‌ها
+            </NavLink>
           </li>
           <li>
-            <NavLink to="/teachers">اساتید</NavLink>
+            <NavLink to="/teachers">
+              اساتید
+            </NavLink>
           </li>
           <li>
-            <NavLink to="/contact">ارتباط با ما</NavLink>
+            <NavLink to="/contact">
+              ارتباط با ما
+            </NavLink>
           </li>
           <li>
-            <NavLink to="/articles">اخبار مقالات</NavLink>
+            <NavLink to="/articles">
+              اخبار مقالات
+            </NavLink>
           </li>
         </ul>
         <div className="flex justify-between items-center gap-4">
           <Link className="bg-white h-[50px] w-[50px] rounded-full hidden sm:flex items-center justify-center">
             <Badge
-              content={bagIconNum > 0 ? bagIconNum : null}
+              content={
+                bagIconNum > 0
+                  ? bagIconNum
+                  : null
+              }
               color="primary"
               shape="circle"
               placement="top-right"
@@ -82,14 +106,38 @@ function MyNavbar() {
       </div>
 
       <NavbarMenu className="bg-[#e7f0fc] space-y-2">
-        <NavMenuLink target="" title="ورود به حساب کاربری" />
-        <NavMenuLink target="" title="پنل کاربری" />
-        <NavMenuLink target="" title="سبد خرید" />
-        <NavMenuLink target="" title="دوره‌ها" />
-        <NavMenuLink target="" title="اساتید" />
-        <NavMenuLink target="" title="ارتباط با ما" />
-        <NavMenuLink target="" title="اخبار مقالات" />
-        <NavMenuLink target="" title="خروج" />
+        <NavMenuLink
+          target=""
+          title="ورود به حساب کاربری"
+        />
+        <NavMenuLink
+          target=""
+          title="پنل کاربری"
+        />
+        <NavMenuLink
+          target=""
+          title="سبد خرید"
+        />
+        <NavMenuLink
+          target=""
+          title="دوره‌ها"
+        />
+        <NavMenuLink
+          target=""
+          title="اساتید"
+        />
+        <NavMenuLink
+          target=""
+          title="ارتباط با ما"
+        />
+        <NavMenuLink
+          target=""
+          title="اخبار مقالات"
+        />
+        <NavMenuLink
+          target=""
+          title="خروج"
+        />
       </NavbarMenu>
     </Navbar>
   );
