2c06c4fbe65926aebd23ab51cea242f1ef304c5c
mhamddyn07 <139611782+mhamddyn07@users.noreply.github.com>
2024-10-09 16:18:03 +0330

    Update routes.jsx

--- src/routes.jsx
+++ src/routes.jsx
@@ -1,6 +1,7 @@
 import Articles from "./pages/Articles";
 import Courses from "./pages/Courses";
 import Landing from "./pages/Landing";
+import LoginCodeVerifiction from "./pages/Login/LoginCodeVerifiction";
 import Login from "./pages/Login";
 import Teachers from "./pages/Teachers";
 import Contact from "./pages/Contact";
@@ -8,12 +9,42 @@ import ArticleDetails from "./pages/ArticleDetails";
 
 const routes = [
   { path: "/", element: <Landing /> },
-  { path: "/courses", element: <Courses /> },
-  { path: "/articles", element: <Articles /> },
-  { path: "/articles/:id", element: <ArticleDetails /> },
-  { path: "/teachers", element: <Teachers /> },
-  { path: "/contact", element: <Contact /> },
-  { path: "/login", element: <Login /> },
+  {
+    path: "/courses",
+    element: <Courses />,
+  },
+  {
+    path: "/articles",
+    element: <Articles />,
+  },
+  {
+    path: "/articles/:id",
+    element: <ArticleDetails />,
+  },
+  {
+    path: "/teachers",
+    element: <Teachers />,
+  },
+  {
+    path: "/contact",
+    element: <Contact />,
+  },
+  {
+    path: "/login",
+    element: <Login />,
+  },
+  {
+    path: "/LoginCodeVerifiction",
+    element: <LoginCodeVerifiction />,
+  },
+  {
+    path: "/login",
+    element: <Login />,
+  },
+  {
+    path: "/login",
+    element: <Login />,
+  },
 ];
 
 export default routes;
