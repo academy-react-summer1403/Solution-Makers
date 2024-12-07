0e7649c8ca83ed7a6119234200b0f28b095f6250
Reza <akbarioccupation@gmail.com>
2024-12-07 18:14:18 +0330

    .

--- src/components/common/Comparison/CompCard/index.jsx
+++ src/components/common/Comparison/CompCard/index.jsx
@@ -3,7 +3,7 @@ import React from "react";
 const CompCard = ({ title, image }) => {
   return (
     <>
-      <div className="flex justify-between items-center text-center border-b-1 border-primary-400 my-2">
+      <div className="flex justify-between items-center text-center border-b-1 border-primary-400 my-2 h-[30px]">
         <div className="bg-primary-100 rounded-full w-7 h-7 flex items-center">
           <img
             src={image}
--- src/components/common/Comparison/CompDetail/index.jsx
+++ src/components/common/Comparison/CompDetail/index.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const CompDetail = ({ detail }) => {
   return (
     <>
-      <div className="flex justify-center items-center text-center my-2">{detail}</div>
+      <div className="flex justify-center items-center text-center my-2 h-[30px]">{detail}</div>
     </>
   )
 }
